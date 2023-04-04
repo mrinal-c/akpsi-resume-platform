7a79e0fc8aadced19d21dbd8b4fbaaaad11a1ef1
Mrinal <cmrinal16@gmail.com>
2023-04-04 17:54:13 -0400

    gitignore

--- mern/server/server.js
+++ mern/server/server.js
@@ -4,9 +4,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.raw({type: 'application/pdf', limit: '50mb'}));
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-const cors = require("cors");
-app.use(cors());
-app.use(express.json());
 app.use(require("./routes/index"));
 const {getAdminKey} = require("./routes/admin");
 // get driver connection
